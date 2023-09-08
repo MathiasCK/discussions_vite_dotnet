711b0b6a1da0b1ffcb3e9c4c874757c2a090b794
mathiasck <mathias.ck2000@gmail.com>
2023-09-08 14:48:47 +0200

    Init server

---
+++ server/Controllers/DiscussionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DiscussionsController : Controller
+{
+    private readonly ILogger<DiscussionsController> _logger;
+
+    public DiscussionsController(ILogger<DiscussionsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("Hello world");
+    }
+}
+
---
+++ server/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
+
