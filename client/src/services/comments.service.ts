04e41569498453fe536c611d0a238231ca0da52f
mathiasck <mathias.ck2000@gmail.com>
2023-09-19 15:18:47 +0200

    Better error handling

--- client/src/services/comments.service.ts
+++ client/src/services/comments.service.ts
@@ -1,40 +1,65 @@
 import { actions } from "../state";
 import { AppPage, Comment } from "../types";
-import { fetcher } from "../utils/fetcher";
 import { displayPopup } from "../utils/popup";
 import { fetchDiscussion } from "./discussions.service";
 
+const commentsUrl = "http://localhost:5000/api/comments";
+
 export const createComment = async (comment: Comment) => {
-  await fetcher("http://localhost:5000/api/comments", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify(comment),
-  });
-  await fetchDiscussion(comment.discussionId);
-  actions.setPage(AppPage.Detail);
+  try {
+    actions.startLoading();
+
+    const response = await fetch(commentsUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(comment),
+    });
+
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      actions.stopLoading();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    actions.stopLoading();
+    await fetchDiscussion(comment.discussionId);
+    actions.setPage(AppPage.Detail);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+  }
 };
 
 export const deleteComment = async (id: string, discussionId: string) => {
-  const response = await fetch(`http://localhost:5000/api/comments/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify(id),
-  });
+  try {
+    actions.startLoading();
 
-  if (!response.ok) {
-    const { status } = response;
-    const message = await response.text();
-    displayPopup(`${status} : ${message}`);
-    return;
+    const response = await fetch(`http://localhost:5000/api/comments/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(id),
+    });
+
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      actions.stopLoading();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    actions.stopLoading();
+    await fetchDiscussion(discussionId);
+    actions.removeComment();
+    actions.setPage(AppPage.Detail);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
   }
-
-  await fetchDiscussion(discussionId);
-  actions.removeComment();
-  actions.setPage(AppPage.Detail);
 };
--- client/src/services/discussions.service.ts
+++ client/src/services/discussions.service.ts
@@ -1,84 +1,142 @@
 import { actions } from "../state";
 import { AppPage, Discussion } from "../types";
-import { fetcher } from "../utils/fetcher";
 import { displayPopup } from "../utils/popup";
 
+const discussionsUrl = "http://localhost:5000/api/discussions";
+
 export const fetchDiscussions = async () => {
-  const discussions: Array<Discussion> = await fetcher(
-    "http://localhost:5000/api/discussions",
-    {
+  try {
+    const response = await fetch(discussionsUrl, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-    },
-  );
-  actions.setDiscussions(discussions);
+    });
+
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    const discussions: Array<Discussion> = await response.json();
+    actions.setDiscussions(discussions);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+  }
 };
 
 export const fetchDiscussion = async (id: string) => {
-  const discussion: Discussion = await fetcher(
-    `http://localhost:5000/api/discussions/${id}`,
-    {
+  try {
+    const response = await fetch(`${discussionsUrl}/${id}`, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-    },
-  );
-  actions.setDiscussion(discussion);
+    });
+
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    const discussion: Discussion = await response.json();
+    actions.setDiscussion(discussion);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+  }
 };
 
 export const createDiscussion = async (discussion: Discussion) => {
-  const createdDiscussion: Discussion = await fetcher(
-    `http://localhost:5000/api/discussions`,
-    {
+  try {
+    actions.startLoading();
+    const response = await fetch(discussionsUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
       body: JSON.stringify(discussion),
-    },
-  );
+    });
 
-  actions.setDiscussion(createdDiscussion);
-  actions.setPage(AppPage.Detail);
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      actions.stopLoading();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    const createdDiscussion: Discussion = await response.json();
+
+    actions.stopLoading();
+    actions.setDiscussion(createdDiscussion);
+    actions.setPage(AppPage.Detail);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+    actions.stopLoading();
+  }
 };
 
 export const updateDiscussion = async (discussion: Discussion) => {
-  const updatedDiscussion: Discussion = await fetcher(
-    `http://localhost:5000/api/discussions/${discussion.id}`,
-    {
+  try {
+    actions.startLoading();
+    const response = await fetch(`${discussionsUrl}/${discussion.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
       body: JSON.stringify(discussion),
-    },
-  );
-  actions.setDiscussion(updatedDiscussion);
-  actions.setPage(AppPage.Detail);
+    });
+
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      actions.stopLoading();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    const updatedDiscussion: Discussion = await response.json();
+
+    actions.stopLoading();
+    actions.setDiscussion(updatedDiscussion);
+    actions.setPage(AppPage.Detail);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+    actions.stopLoading();
+  }
 };
 
 export const deleteDiscussion = async (id: string) => {
-  const response = await fetch(`http://localhost:5000/api/discussions/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify(id),
-  });
+  try {
+    actions.startLoading();
+    const response = await fetch(`${discussionsUrl}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(id),
+    });
 
-  if (!response.ok) {
-    const { status } = response;
-    const message = await response.text();
-    displayPopup(`${status} : ${message}`);
-    return;
+    if (!response.ok) {
+      const { status } = response;
+      const message = await response.text();
+      actions.stopLoading();
+      return displayPopup(`${status} : ${message}`);
+    }
+
+    actions.stopLoading();
+    actions.removeDiscussion();
+    actions.setPage(AppPage.Discussions);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    displayPopup(e.message);
+    actions.stopLoading();
   }
-
-  actions.removeDiscussion();
-  actions.setPage(AppPage.Discussions);
 };
