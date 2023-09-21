7161e95b140b4256589858a1b807eebfb8c60c91
mathiasck <mathias.ck2000@gmail.com>
2023-09-21 10:49:48 +0200

    Add tests for verifyTokenPayload

--- client/src/test/token.test.ts
+++ client/src/test/token.test.ts
@@ -1,10 +1,19 @@
 import { expect, test, describe } from "vitest";
+import jwt from "jsonwebtoken";
+
+const genJWT = (expirationTime: number) =>
+  jwt.sign(
+    {
+      sub: "webtoken",
+      exp: expirationTime,
+    },
+    "super-secret-key",
+  );
 
 describe("verifyTokenPayload", () => {
   test("returns true if token is valid", () => {
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImMyYThiNDg4LWQxNmMtNDVjNC05NDhlLTE4N2Q0YTZjMGE0YyIsIm5iZiI6MTY5NTIzNjE4MywiZXhwIjoxNjk1MjM5NzgzLCJpYXQiOjE2OTUyMzYxODN9.2THSlkBuZHzKMgwnHh9ZSm126aBOn9Mm0qhloF6qHYM";
-
+    const validTimeStamp = Math.floor(Date.now() / 1000) - 3600;
+    const token = genJWT(validTimeStamp);
     const currentTimeSeconds = Date.now() / 1000;
 
     const { exp: tokenExp } = JSON.parse(atob(token.split(".")[1]));
@@ -12,5 +21,17 @@ describe("verifyTokenPayload", () => {
     const result = tokenExp < currentTimeSeconds;
 
     expect(result).toBe(true);
+  });
+  test("returns false if token is expired", () => {
+    const unvalidTimeStamp = Math.floor(Date.now() / 1000) + 3600;
+    const token = genJWT(unvalidTimeStamp);
+
+    const currentTimeSeconds = Date.now() / 1000;
+
+    const { exp: tokenExp } = JSON.parse(atob(token.split(".")[1]));
+
+    const result = tokenExp < currentTimeSeconds;
+
+    expect(result).toBe(false);
   });
 });
